87da7174db9f57d338a2f27c28407c656f62059b
kun <591619648@qq.com>
2019-04-15 05:21:41 +0800

    Signed-off-by: kun <“591619648@qq.com>

--- test1.pl
+++ test1.pl
@@ -250,14 +250,52 @@ use autodie;
 #	}
 #}
 
+#.和\N表示匹配除换行符之外的任意单个字符
+#$_ = "yabba dabba doo!";
+#if(/doo./){  
+#	print "\tMatch\n";
+#}
+#else{
+#	print "\tDoesn't match\n";
+#	}
 
-$_ = "yabba dabba doo!";
-if(/doo./){  #.表示匹配除换行符之外的任意单个字符
-	print "\tMatch\n";
-}
-else{
-	print "\tDoesn't match\n";
-	}
+#b{2}表示匹配b出现2次  等于bb	
+#$_ = "yabba dabbna doo!";
+#if(/ab{2}a/){  
+#	print "\tMatch\n";
+#}
+#else{
+#	print "\tDoesn't match\n";
+#	}
 
+#(.)\1 匹配两个连续出现同样的字符
+#$_ = "yabba da1a doafo!";
+#if(/a(.)\1a/){ 
+#	print "\tMatch\n";
+#}
+#else{
+#	print "\tDoesn't match\n";
+#	}
 
+#\g{-1} 等于 \-1 离他左边最进一个分组
+#$_ = "xax11bb";
+#if(/(.)(.)\g{-1}11/){ 
+#	print "\tMatch\n";
+#}
+#else{
+#	print "\tDoesn't match\n";
+#	}
 
+# |表示或 要么左边 要么右边 ( |\t)+ 表示要么1个到多个空格 要么1个到多个\t
+#$_ = "aa";
+#if(/( |\t)+aa/){ 
+#	print "\tMatch\n";
+#}
+#else{
+#	print "\tDoesn't match\n";
+#	}
+
+# \Z字符串结尾 if判断可以在print 后使用
+#while (<STDIN>){
+#	print "$_\n" if /\.png\Z/;
+#}
