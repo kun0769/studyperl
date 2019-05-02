95836ea426bfb475af132e91714c43c420a7b05f
kun <591619648@qq.com>
2019-05-02 22:54:16 +0800

    Signed-off-by: kun <“591619648@qq.com>

--- test1.pl
+++ test1.pl
@@ -420,3 +420,24 @@ use autodie;
 #	print " $_ ==> $last_name{$_}\n";
 #}
 
+
+#my $in = $ARGV[0];
+#if(! defined $in){
+#	die "Usage: $0 filename";
+#}
+#
+#my $out = $in;
+#$out =~ s/(\.\w+)?$/.out/;
+#
+#if(! open my $in_fh, '<', $in){
+#	die "Can't open '$in': $!";
+#}
+#
+#if(! open my $out_fh, '>', $out){
+#	die "Can't write '$out': $!";
+#}
+#
+#while(<$in_fh>){
+#	s/Fred/Larry/ig;
+#	print $out_fh $_;
+#}
